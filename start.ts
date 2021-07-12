d86442a359cb47d0ec6b2012062a7d15789bfaad
karel.suchomel <karelsuchomel@windowslive.com>
2021-07-12 18:00:13 +0200

    Quiet deprecation notice of dependencies from SASS libs. Updated README.md

--- start.ts
+++ start.ts
@@ -1,9 +1,10 @@
 // SCSS
 const scssProcess = Deno.run({
   cmd: [
-    "dart",
     "sass",
     "--watch",
+    "--quiet-deps",
+    "--no-error-css",
     "--style",
     "expanded",
     "scss/index.scss:dist/css/client.css",
